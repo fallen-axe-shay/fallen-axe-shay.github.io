4b286f0a92b65bd877ed4f99b6dbd10a27d1ff7d
fallen-axe-shay <jerry.allan.akshay@gmail.com>
2022-01-14 17:22:09 -0800

    Added shopify-backend-assignment

---
+++ shopify-internship-backend/global/scripts/firebase/firebase-functions.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDutUW0S8hpc-jdph5H7o4kM3kjeRyk9KQ",
+  authDomain: "shopify-internship-5b8ce.firebaseapp.com",
+  databaseURL: "https://shopify-internship-5b8ce-default-rtdb.firebaseio.com",
+  projectId: "shopify-internship-5b8ce",
+  storageBucket: "shopify-internship-5b8ce.appspot.com",
+  messagingSenderId: "737517945675",
+  appId: "1:737517945675:web:abe6b090cc24dfa55bdece",
+  measurementId: "G-2T6RE4J5BH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
